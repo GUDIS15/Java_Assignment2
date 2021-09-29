f2b330e1f8202d84ac49d7373c16e017939fa378
GUDIS15 <91594332+GUDIS15@users.noreply.github.com>
2021-09-29 13:34:03 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,26 @@
+package Assignment2.java;
+
+public class Q1 {
+	public static void main(String[] args) {
+		 
+        // create an array
+        int[] array = { 1, 2, 3, 4, 5};
+ 
+        // getting array length
+        int length = array.length;
+ 
+        // default sium value.
+        int sum = 0;
+ 
+        // sum of all values in array using for loop
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+ 
+        double average = sum / length;
+         
+        System.out.println("Average of array : "+average);
+ 
+    }
+
+}
---
+++ Q2.java
@@ -0,0 +1,14 @@
+package Assignment2.java;
+
+public class Q2 {
+	public static void main(String args[]) {
+		int n = 187;
+		System.out.print("Odd Numbers from 79 to "+n+" are: ");
+		for (int i = 79; i <= n; i++) {
+		   if (i % 2 != 0) {
+			System.out.print(i + " ");
+		   }
+		}
+	   }
+
+}
